392da1183d61bb5bf3f1dd0f4629b685fcf0158d
Ricardo Guerrero <richi_gro@hotmail.com>
2018-08-13 22:09:29 -0500

    debugging test reponse from zillow api reponse

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const apiKey = "X1-ZWz1gl3l1q9s7f_8nu8h";
 const url = "https://www.zillow.com/webservice/GetRegionChildren.htm";
 
 function getDataFromApi(userSearch, callback){
-    const query = {
-        'zws-id': apiKey,
-        state: userSearch
-    };
-
     $.ajax({
-        url: url
-     });
+        type: "GET",
+        url: url,
+        data:`{'zwa-id': ${apiKey}, state: ${userSearch}, Limit-warning : ${true} }`; 
+        dataType: "xml",
+        success: callback(data)
+    });
 }
 
 function printResult(data) {
