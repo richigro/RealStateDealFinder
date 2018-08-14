5fc1298de20d7e6fbf41f8fc4a0a568bd66d7462
Ricardo Guerrero <richi_gro@hotmail.com>
2018-08-13 23:17:10 -0500

    syntax error

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const apiKey = "X1-ZWz1gl3l1q9s7f_8nu8h";
 const zillowUrl = "https://www.zillow.com/webservice/GetRegionChildren.htm";
-const corsProxy = "https://crossorigin.me/";
+const corsProxy = "https://cors-anywhere.herokuapp.com/";
 const corsEnabledUrl = `${corsProxy}${zillowUrl}`;
 console.log(corsEnabledUrl);
+
+function setHeader(xhrObj){
+    xhr.setRequestHeader("Origin", `${zillowUrl}`);
+}
+
 
 function getDataFromApi(userSearch, callback){
     $.ajax({
         type: "GET",
+        beforeSend: setHeader(),
         url: corsEnabledUrl,
         // data: `{'zwa-id': ${apiKey}, state: ${userSearch}, Limit-warning : ${true} }`, 
         dataType: "xml",
