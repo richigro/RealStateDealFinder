0d9c7a1a776e182ba33640951c340f90b553ece9
Ricardo Guerrero <richi_gro@hotmail.com>
2018-08-13 21:22:31 -0500

    delted template code and added a test index.js file

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-alert("connected succesfully to index.js");
+const apiKey = "X1-ZWz1gl3l1q9s7f_8nu8h";
+const url = "https://www.zillow.com/webservice/GetRegionChildren.htm";
+
+function getDataFromApi(userSearch, callback){
+    const query = {
+        "zwd-id": apiKey,
+        state: userSearch
+    };
+
+    $.ajax({
+        url: url
+     });
+}
+
+function printResult(data) {
+    console.log(data);
+}
+
+ function appInstance(){
+     // run fucntions
+    const search = "" 
+    getDataFromApi();
+ }
+
+ $(appInstance);
