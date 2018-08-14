008feb2e501a04d18ac1051a515445f81f6c6fc2
Ricardo Guerrero <richi_gro@hotmail.com>
2018-08-13 22:48:37 -0500

    syntax error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getDataFromApi(userSearch, callback){
     $.ajax({
         type: "GET",
         url: corsEnabledUrl,
-        data: `{'zwa-id': ${apiKey}, state: ${userSearch}, Limit-warning : ${true} }`, 
+        // data: `{'zwa-id': ${apiKey}, state: ${userSearch}, Limit-warning : ${true} }`, 
         dataType: "xml",
         success: callback(data),
         error: callback(data)
